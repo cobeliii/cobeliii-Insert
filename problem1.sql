0c8d1621b706cdeff6f86fe60de4f07f7253f459
Newton Jorge <cobeliiimuname@gmail.com>
2024-08-23 21:55:56 +0000

    Completed Lab Insert

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+Insert into song (title, artist) values ('Let it be', 'Beatles');
+Insert into song (title, artist) values ('Hotel California', 'Eagles');
+Insert into song (title, artist) values ('Kashmir', 'Led Zeppelin');
